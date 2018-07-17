3b43bccbf98acb45e89fb7686d480cce79e8917b
Ptashkina Liliya <efekta9@gmail.com>
2018-07-17 21:38:59 +0300

    no message

--- js/common.js
+++ js/common.js
@@ -66,14 +66,14 @@ else{
   fixed sidebar
 ==========================*/
 
-// $(window).scroll(function() {
-// 	if($(this).scrollTop() >= 460) {
-// 		$('.sidebar-container').addClass('sidebar-container_fixed');
-// 	}
-// 	else{
-// 		$('.sidebar-container').removeClass('sidebar-container_fixed');
-// 	}
-// });
+$(window).scroll(function() {
+	if($(this).scrollTop() >= 460) {
+		$('.sidebar-container').addClass('sidebar-container_fixed');
+	}
+	else{
+		$('.sidebar-container').removeClass('sidebar-container_fixed');
+	}
+});
 
 /* ==========================================
   Функции открытия и закрытия сайдбара
