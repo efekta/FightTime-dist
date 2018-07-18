577d1797676a7191b838c53b92e328140608e35c
Ptashkina Liliya <efekta9@gmail.com>
2018-07-18 15:43:55 +0300

    no message

--- js/common.js
+++ js/common.js
@@ -67,7 +67,7 @@ else{
 ==========================*/
 
 $(window).scroll(function() {
-	if($(this).scrollTop() >= 460) {
+	if($(this).scrollTop() >= 260) {
 		$('.sidebar-container').addClass('sidebar-container_fixed');
 	}
 	else{
